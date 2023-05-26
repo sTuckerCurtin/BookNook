843a460c769540881f95fc489e16214f58558241
Samuel Curtin <stuckercurtin@gmail.com>
2023-05-26 16:34:54 -0400

    Added Review resource

--- backend/resources/books.py
+++ backend/resources/books.py
@@ -1,5 +1,21 @@
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 from flask_restful import Resource
-from database.models import db, Car
-from database.schemas import car_schema, cars_schema
+from database.models import db, Review
+from database.models import db, Favorite
+from database.schemas import review_schema, reviews_schema
+from database.schemas import favorite_schema, favorites_schema
+
+
+class AllReviewResource(Resource):
+    @jwt_required
+    def post(self):
+        user_id = get_jwt_identity()
+        form_data = request.get_json()
+        new_review = review_schema.load(form_data)
+        new_review.user_id = user_id
+        db.session.add(new_review)
+        db.session.commit()
+        return review_schema.dump(new_review), 201
+    
+
